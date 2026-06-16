645a19e11333e4dd396c6d5b2e8400b8d69f205a
DEV-COM <kwp@tlen.pl>
2026-06-16 10:53:37 +0200

    nowe pliki 16.06

--- config/settings.py
+++ config/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -76,6 +76,14 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "postgresql_psycopg2": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "devboard",
+        "USER": "postgres",
+        "PASSWORD": "secret",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -102,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pl"
 
 TIME_ZONE = "UTC"
 
