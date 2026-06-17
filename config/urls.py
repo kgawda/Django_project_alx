08c45bc46b24a438dd659fe006dfa21a3d5c7c5c
DEV-COM <kwp@tlen.pl>
2026-06-17 14:56:15 +0200

    nowe pliki 17.06 14:45

--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from devboard import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name="index"),
+    path("", views.ProjectListView.as_view(), name="lista-project"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
