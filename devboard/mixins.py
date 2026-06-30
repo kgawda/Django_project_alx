b8c3da74bf0a9052719fd9221c41bd9c4dc9a4c9
Konrad Gawda <konrad.gawda.opensource@gmail.com>
2026-06-30 14:42:43 +0200

    Add own mixin

---
+++ devboard/mixins.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from devboard.models import Project
+
+class OwnerQuerysetMixin(LoginRequiredMixin):
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
--- devboard/views.py
+++ devboard/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 
 from devboard.forms import ProjectForm, TaskForm
+from devboard.mixins import OwnerQuerysetMixin
 from devboard.models import Project, Task
 
 
@@ -14,25 +15,22 @@ from devboard.models import Project, Task
 # def index(request):
 #     return render(request, "index.html")
 
-class ProjectListView(LoginRequiredMixin, ListView):
+class ProjectListView(OwnerQuerysetMixin, ListView):
     model = Project
     template_name = "devboard/project_list.html"
     context_object_name = "projects"
 
     def get_queryset(self):
         return (
-            Project.objects.filter(owner=self.request.user)
+            super().get_queryset()
             .annotate(task_count=Count("tasks"))
             .order_by("-created_at")
         )
 
-class ProjectDetailView(LoginRequiredMixin, DetailView):
+class ProjectDetailView(OwnerQuerysetMixin, DetailView):
     model = Project
     template_name = "devboard/project_detail.html"
     context_object_name = "project"
-
-    def get_queryset(self):
-        return Project.objects.filter(owner=self.request.user)
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
