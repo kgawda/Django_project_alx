61aa60a4886e900756d5242a73676ac5e0885888
Konrad Gawda <konrad.gawda.opensource@gmail.com>
2026-07-01 10:44:39 +0200

    Add status change signal print

--- devboard/signals.py
+++ devboard/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from devboard.models import Task
 
@@ -9,3 +9,16 @@ def save_done_info(sender, instance, **kwargs):
 @receiver(post_save, sender=Task, dispatch_uid="save_done_info2_on_taks")
 def save_done_info2(sender, instance, **kwargs):
     print(f"Zapisano {instance} do bazy!")
+
+@receiver(pre_save, sender=Task, dispatch_uid="task_status_change_check")
+def check_old_status(sender, instance, **kwargs):
+    if instance.pk:
+        status = sender.objects.filter(pk=instance.pk).first().status
+        instance._old_status = status
+
+@receiver(post_save, sender=Task, dispatch_uid="task_status_change_log")
+def log_status_change(sender, instance, created, **kwargs):
+    if created:
+        return
+    if hasattr(instance, "_old_status") and (instance._old_status != instance.status):
+        print(f"Task {instance.title} status changed: {instance._old_status} -> {instance.status}")
